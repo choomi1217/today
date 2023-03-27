511ef71f610228bffd64dc2a976e9ce1444c7a21
o_oMi <52321403+choomi1217@users.noreply.github.com>
2023-03-27 15:56:37 +0900

    Delete 2022/11/JAVA/src directory

--- 2022/11/JAVA/src/daily_inspector/integrator_inspector/requestor/Requestor.java
+++
@@ -1,9 +0,0 @@
-package daily_inspector.integrator_inspector.requestor;
-
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-
-public interface Requestor {
-    public void connect() throws IOException, NoSuchAlgorithmException, KeyManagementException;
-}
